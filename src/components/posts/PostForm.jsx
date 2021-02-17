5ed4a43b738f95285ada0c070eb5c7dbed230d55
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-17 18:09:18 -0500

    set up local state for title and body and prepare onSubmit

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function PostForm() {
+  
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault;
+  };
 
   return (
-    // <form onSubmit={handleSubmit}>
-    <form>
-      <input 
-        type="text" 
-        placeholder="Title" 
-      />
-      <br />
-      <textarea 
-        placeholder="Body" 
-        rows="10" 
-        cols="50"></textarea>
-      <br />
+    <>
+      <h3>Add a Blog Post</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Title" 
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <br />
+        <textarea 
+          placeholder="Body" 
+          rows="10" 
+          cols="50"
+          value={body}
+          onChange={({ target }) => setBody(target.value)}
+        ></textarea>
+        <br />
 
-      <button>Post</button>
-    </form>
+        <button>Post</button>
+      </form>
+    </>
   );
 }
 
