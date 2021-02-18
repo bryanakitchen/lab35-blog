f158f36565c17bcd3036e34a7968f0baa8be3500
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-17 19:15:43 -0500

    add handleClick for deleting post by name

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from '../../state/BlogProvider';
+import { deletePost } from '../../actions/postActions';
 
 export default function Post({ title, body }) {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(deletePost(title));
+  };
+  
   return (
     <div>
       <h4>{title}</h4>
       <p>{body}</p>
-      <button>Delete</button>
+      <button onClick={handleClick} >Delete</button>
     </div>
   );
 }
