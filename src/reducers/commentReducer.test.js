9c035d7c61326eed3f66dd500c366bc6b324b10b
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-18 21:00:17 -0500

    add delete comment reducer and reducer test

--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -1,5 +1,5 @@
 import reducer from '.';
-import { createComment } from '../actions/commentActions';
+import { createComment, deleteComment } from '../actions/commentActions';
 
 describe('Comment Reducer', () => {
   it('Creates a comment using CREATE_COMMENT action in commentReducer', () => {
@@ -20,6 +20,30 @@ describe('Comment Reducer', () => {
         body: 'Today is Thursday'
       }],
       comments: { 0: ['This is my comment'] }
+    });
+  });
+
+  // eslint-disable-next-line max-len
+  it.only('Deletes a comment using DELETE_COMMENT action in commentReducer', () => {
+    const state = {
+      posts: [{
+        title: 'Friday',
+        body: 'Tomorrow is Friday'
+      }],
+      comments: { 0: ['This is my Friday comment'] }
+    };
+
+    const action = deleteComment(0, 0);
+    // { type: 'DELETE_COMMENT', payload: { postIndex: 0, textIndex: 0 } }
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      posts: [{
+        title: 'Friday',
+        body: 'Tomorrow is Friday'
+      }],
+      comments: { 0: [] }
     });
   });
 });
